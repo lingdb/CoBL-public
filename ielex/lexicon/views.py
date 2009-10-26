d7e08bff847775a9178e4e3c0ed853397f953fc5
Michael Dunn <michael.dunn@mpi.nl>
2009-10-26 21:04:18 +0100

    basic nexus export

--- ielex/lexicon/views.py
+++ ielex/lexicon/views.py
@@ -4,21 +4,57 @@ from django.http import HttpResponse
 from ielex.lexicon.models import *
 
 def write_nexus(response):
+    start_time = time.time()
+
     # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(mimetype='text/plain')
     # response['Content-Disposition'] = 'attachment; filename=ielex.nex'
-    print>>response, "#NEXUS"
-    print>>reponse, "[ File generated: %s ]" % time.strftime("%Y-%m-%d %H:%M:%S",
-            time.localtime()) 
-    languages = Language.objects.filter(id__in=[1,2,3])
-    cognate_classes = CognateSet.objects.filter(id__in=range(1,21))
+    print>>response, dedent("""\
+        #NEXUS
+
+        [ Citation:                                                        ]
+        [   Dunn, Michael; Ludewig, Julia. 2009. IELex (Indo-European      ]
+        [   Lexicon) Database. Max Planck Institute for Psycholinguistics, ]
+        [   Nijmegen.                                                      ]
+
+        [ File generated: %s ]
+        """ % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+    # get data together
+    languages = Language.objects.all()
+    language_names = languages.values_list("ascii_name", flat=True)
+    max_len = max([len(l) for l in language_names])
+    cognate_class_ids = CognateSet.objects.all().values_list("id", flat=True)
+    data = {}
+    for cc in cognate_class_ids:
+        data[cc] = CognateSet.objects.get(id=cc).lexeme_set.values_list('language', flat=True)
+
+    # start printout
+    print>>response, dedent("""\
+        begin taxa;
+          dimensions ntax=%s;
+          taxlabels %s;
+        end;
+        """ % (len(languages), " ".join(language_names)))
+    print>>response, dedent("""\
+        begin characters;
+          dimensions nchar=%s;
+          format symbols="01";
+          matrix""" % len(data))
+
     for language in languages:
         row = []
-        row.append(language.ascii_name)
-        data = []
-        # cj = CognateJudgement.objects.filter(language=language
-        # CognateSet.objects.filter(lexeme__language=language, id__in=range(1,21))
-        # for cognate_class in cognate_classes.filter(language=language):
-        print>>response, "".join(row)
+        for cc in cognate_class_ids:
+            if language.id in data[cc]:
+                row.append("1")
+            else:
+                row.append("0")
+        print>>response, "    %s %s" % (language.ascii_name.ljust(max_len), "".join(row))
+    print>>response, "  ;\nend;"
 
+    # timing
+    seconds = int(time.time() - start_time)
+    minutes = seconds // 60
+    seconds %= 60
+    print>>response, "[ Processing time: %02d:%02d ]" % (minutes, seconds)
     return response
