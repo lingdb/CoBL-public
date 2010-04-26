a31e628f67250fc6999f72eb8ebc5fd76edd23c2
Michael Dunn <michael.dunn@mpi.nl>
2010-04-26 08:13:53 +0200

    renamed

--- ielex/runserver.sh
+++ ielex/runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # If we boot up the server in the background then open the browser, zombie
-# python processes get left behind.
+# python processes get left behind. So we do it the other way around...
 
 # Step 1: open the browser ten seconds from now
 sh runserver_2.sh &
