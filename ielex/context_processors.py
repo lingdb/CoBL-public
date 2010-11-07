b48996d329b7f92f8f136aee2a3be0d128e4ea35
Michael Dunn <michael.dunn@mpi.nl>
2010-11-07 13:55:44 +0100

    minor

--- ielex/context_processors.py
+++ ielex/context_processors.py
@@ -1,17 +1,20 @@
-from ielex.settings import VERSION
 """Writing your own context processors
 
-A context processor has a very simple interface: It's just a Python function
-that takes one argument, an HttpRequest object, and returns a dictionary that
-gets added to the template context. Each context processor must return a
-dictionary.
+A context processor is a Python function that takes one argument, an
+HttpRequest object, and returns a dictionary that gets added to the template
+context. Each context processor must return a dictionary.
 
-Custom context processors can live anywhere in your code base. All Django cares
-about is that your custom context processors are pointed-to by your
+Custom context processors can live anywhere in the code base. All Django cares
+about is that your custom context processors are pointed-to by the
 TEMPLATE_CONTEXT_PROCESSORS setting."""
 
+from ielex.settings import VERSION
+
 def version(request):
+    """Provides a {{ version }} tag"""
     return {"version":VERSION}
 
 def current_url(request):
+    """The {{ current_url }} tag is used as a link target to force reloading of
+    a page (e.g. to dismiss a message)"""
     return {"current_url":request.get_full_path()}
