8964cac37023a5c08d7bf24625d3cbf7c31fd469
Michael Dunn <michael.dunn@mpi.nl>
2010-11-09 07:45:49 +0100

    development version of local settings

---
+++ ielex/dev/local_settings.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# SECRET_KEY should be kept here!
+
+project_long_name = "Indo-European Lexical Cognacy Database"
+project_short_name = "IELEX"
+project_description = """"""
+acknowledgements = """\
+<p>The icons used in this project are taken from the set ‘Sweetie BasePack’:
+<table>
+  <tr><td>Author:</td><td>Joseph North</td></tr>
+  <tr><td>Licence:</td><td><a href="http://creativecommons.org/licenses/by-sa/3.0/">Attribution-ShareAlike 3.0</a></td></tr>
+  <tr><td>Website:</td><td><a href="http://sweetie.sublink.ca">sweetie.sublink.ca</a></td></tr>
+</table>
+</p>"""
+
+
+
+
+# vim:ft=python
