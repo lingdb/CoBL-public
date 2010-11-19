9ff432507721f6649f023fe66ad7618e53c7224d
Michael Dunn <michael.dunn@mpi.nl>
2010-11-19 23:09:50 +0100

    setup_environment announces database name correctly

--- ielex/dev/setup_environment.py
+++ ielex/dev/setup_environment.py
@@ -2,9 +2,12 @@
 # %run setup_environment.py
 import sys
 import os
-print "-> I E L E X"
 from django.core.management import setup_environ
 sys.path.append(os.path.abspath("../.."))
 from ielex import settings
 setup_environ(settings)
 from ielex.lexicon.models import *
+name = settings.project_short_name
+print "+%s+" % ("-" * (len(name) + 2))
+print "|", name, "|"
+print "+%s+" % ("-" * (len(name) + 2))
