b6c72fd5fadc26f7b817d8b48be1198082e1ea73
Michael Dunn <michael.dunn@mpi.nl>
2010-11-20 18:31:20 +0100

    cull old, obsolete files from top level dir

--- new_wals_languages.sql
+++
@@ -1,8 +0,0 @@
-begin transaction;
-insert into languages (lang_code, lang_name, family, iso_code) values
-  ("xhit", "Hittite", "Indo-European", "hit");
-insert into languages (lang_code, lang_name, family, iso_code) values
-  ("xxto", "Tocharian A", "Indo-European", "xto");
-insert into languages (lang_code, lang_name, family, iso_code) values
-  ("xtxb", "Tocharian B", "Indo-European", "txb");
-commit;
