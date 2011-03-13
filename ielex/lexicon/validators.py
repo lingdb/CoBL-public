e4580305b2f1fe51658f7c9cb2f1e2b7c06d31a5
Michael Dunn <michael.dunn@mpi.nl>
2011-03-13 08:33:52 +0100

    fixed illegal regex

--- ielex/lexicon/validators.py
+++ ielex/lexicon/validators.py
@@ -9,7 +9,7 @@ from django.core.validators import ValidationError
 
 def suitable_for_url(value):
     # cf also clean_ascii_name function in forms.py
-    regex = re.compile(r"[a-zA-Z0-9_-.]+")
+    regex = re.compile(r"[a-zA-Z0-9_.-]+")
     match = regex.match(value)
     if match.start() > 0 or match.end() < len(value):
         raise ValidationError("Meaning.gloss does not match regex pattern")
