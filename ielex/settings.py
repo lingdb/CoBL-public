d826e3fcfe2138c1f33886bd773a96b5b84d7405
Michael Dunn <michael.dunn@mpi.nl>
2011-08-16 22:13:07 +0200

    cache is_loanword method

--- ielex/settings.py
+++ ielex/settings.py
@@ -123,6 +123,7 @@ if DEBUG:
         MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
         INTERNAL_IPS = ('127.0.0.1',)
         INSTALLED_APPS += ('debug_toolbar',)
+        DEBUG_TOOLBAR_CONFIG = {'INTERCEPT_REDIRECTS':False}
     except ImportError:
         pass
 
