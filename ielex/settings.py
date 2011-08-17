1ea31eadbe10067004096faa2473e53235f3d672
Michael Dunn <michael.dunn@mpi.nl>
2011-08-17 15:02:38 +0200

    handle deprecation warning

--- ielex/settings.py
+++ ielex/settings.py
@@ -61,10 +61,10 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     'ielex.context_processors.configuration',
     'ielex.context_processors.navigation',
