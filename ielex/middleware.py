4484a96fb9a72cd8fcf33fd9e4707c95b11e235c
Michael Dunn <michael.dunn@mpi.nl>
2012-05-24 14:18:53 +0200

    added middleware.py

---
+++ ielex/middleware.py
@@ -0,0 +1,12 @@
+from os import linesep
+from django.utils.encoding import force_unicode
+
+def strip_blank_lines(value):
+    return linesep.join([s for s in force_unicode(value).splitlines() if s.strip()])
+
+class NoBlankLinesMiddleware(object):
+    def process_response(self, request, response):
+        if 'text/html' in response['Content-Type']:
+            response.content = strip_blank_lines(response.content)
+        return response
+
