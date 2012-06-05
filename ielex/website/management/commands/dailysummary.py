ecce0b183caea1ed5d379c54a0ba24c2335a4ff3
Michael Dunn <michael.dunn@mpi.nl>
2012-06-05 23:46:44 +0200

    added main_admins default to dailysummary command

--- ielex/website/management/commands/dailysummary.py
+++ ielex/website/management/commands/dailysummary.py
@@ -1,22 +1,29 @@
 from __future__ import print_function
 from optparse import make_option
-import datetime
+from datetime import datetime, timedelta
+import sys
+from StringIO import StringIO
 from django.core.management.base import NoArgsCommand, CommandError
+from django.core.mail import mail_admins
 from reversion.models import Revision
+from lexicon.models import *
 
 class Command(NoArgsCommand):
     help="""Report the changes to the database over the previous N days"""
     option_list = (
-            make_option("-n", "--dry-run", dest="dry_run",
+            make_option("-d", "--dry-run", dest="dry_run",
                 action="store_true", default=False,
-                help="Report daily summary to stdout"),
+                help="Report daily summary to stdout "\
+                "[default: email it to admins]"),
             make_option("-e", "--no-empty", dest="include_empty",
                 action="store_false", default=True,
-                help="Suppress reports when there have been no changes"),
-            make_option("-d", "--num-days", dest="num_days",
+                help="Suppress reports when there have been no changes "\
+                "[default: show empty]"),
+            make_option("-n", "--num-days", dest="num_days",
                 action="store", default=1, type=int,
                 metavar="N",
-                help="Report the activity for the previous N days"),
+                help="Report the activity for the previous N days "\
+                "[default: 1]"),
             )
 
     def run_from_argv(self, argv):
@@ -34,12 +41,14 @@ class Command(NoArgsCommand):
         return
 
     def handle(self, **options):
-        # TODO make a filter so that this report doesn't tell too much about
-        # e.g. LanguageListOrder changes
-        start_date = datetime.datetime.now() - datetime.timedelta(
-                days=options["num_days"])
+        if options["dry_run"]:
+            fileobj=sys.stdout
+        else:
+            fileobj=StringIO()
+        boring_models = [LanguageListOrder]
+        start_date = datetime.now() - timedelta(days=options["num_days"])
         recent_changes = Revision.objects.filter(
-                date_created__gt=start_date).order_by("-id")
+                date_created__gt=start_date).order_by("date_created")
         if recent_changes or options["include_empty"]:
             for revision in recent_changes:
                 timestamp = revision.date_created.strftime(
@@ -50,14 +59,20 @@ class Command(NoArgsCommand):
                             user.username)
                 else:
                     username = user.username
-                print(timestamp, user)
+                print(timestamp, user, file=fileobj)
                 for version in revision.version_set.all():
-                    try:
-                        print(" ", version.get_type_display(),
-                                repr(version.get_object_version().object))
-                    except:
-                        print(" ", version.get_type_display(),
-                                "OBJECT UNAVAILABLE")
-
+                    if version.content_type.model_class() not in boring_models:
+                        try:
+                            print(" ", version.get_type_display(),
+                                    repr(version.get_object_version().object),
+                                    file=fileobj)
+                        except:
+                            print(" ", version.get_type_display(),
+                                    "OBJECT UNAVAILABLE", file=fileobj)
+        if not options["dry_run"]:
+            fileobj.seek(0)
+            subject = "Daily activity report %s"
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            mail_admins(subject, fileobj.read())
         return
 
