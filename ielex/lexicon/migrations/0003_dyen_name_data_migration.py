64cb93c6f8db8e2cac56085e4df89443d3c52a06
Jakob Runge <sicarius@g4t3.de>
2016-02-04 16:25:13 +0100

    Remove tab identations

--- ielex/lexicon/migrations/0003_dyen_name_data_migration.py
+++ ielex/lexicon/migrations/0003_dyen_name_data_migration.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 def dyenname_to_languagedata(apps, schema_editor):
-	# move (possible) contents of the DyenName table to
-	# Language.data["dyen_name"] in preparation for dropping
-	# DyenName table
-	DyenName = apps.get_model("lexicon", "DyenName")
-	for dnobj in DyenName.objects.all():
-		dnobj.language.data["dyen_name"] = dnobj.name
-		dnobj.language.save()
+    # move (possible) contents of the DyenName table to
+    # Language.data["dyen_name"] in preparation for dropping
+    # DyenName table
+    DyenName = apps.get_model("lexicon", "DyenName")
+    for dnobj in DyenName.objects.all():
+        dnobj.language.data["dyen_name"] = dnobj.name
+        dnobj.language.save()
 
 class Migration(migrations.Migration):
 
@@ -18,5 +18,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-		migrations.RunPython(dyenname_to_languagedata)
+        migrations.RunPython(dyenname_to_languagedata)
     ]
