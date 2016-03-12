63aa14a7d40ec773c0b87b277f4f59a01f022a9f
Jakob Runge <sicarius@g4t3.de>
2016-03-12 13:12:03 +0100

    Pep8 migrations/001{6,7}*.py

--- ielex/lexicon/migrations/0016_languagebranches_armenian.py
+++ ielex/lexicon/migrations/0016_languagebranches_armenian.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 
+
 def forwards_func(apps, schema_editor):
     LanguageBranches = apps.get_model("lexicon", "LanguageBranches")
     print('Adding Armenian LanguageBranch')
@@ -16,7 +17,8 @@ def forwards_func(apps, schema_editor):
 
 def reverse_func(apps, schema_editor):
     LanguageBranches = apps.get_model("lexicon", "LanguageBranches")
-    branch = LanguageBranches.objects.get(family_ix=9, level1_branch_ix=3, level1_branch_name='Armenian')
+    branch = LanguageBranches.objects.get(
+        family_ix=9, level1_branch_ix=3, level1_branch_name='Armenian')
     branch.delete()
 
 
--- ielex/lexicon/migrations/0017_languagebranches_addColors.py
+++ ielex/lexicon/migrations/0017_languagebranches_addColors.py
@@ -17,12 +17,14 @@ setColors = [{'name': 'ALL Indic',        'color': 'E11D21'},
              {'name': 'ALL Romance',      'color': '66FF00'},
              {'name': 'ALL Celtic',       'color': '006301'}]
 
+
 def forwards_func(apps, schema_editor):
     LanguageBranches = apps.get_model("lexicon", "LanguageBranches")
     print('Coloring LanguageBranches…')
-    for set in  setColors:
+    for set in setColors:
         try:
-            branch = LanguageBranches.objects.get(level1_branch_name=set['name'])
+            branch = LanguageBranches.objects.get(
+                level1_branch_name=set['name'])
             branch.hexColor = set['color']
             branch.save()
         except:
@@ -32,9 +34,10 @@ def forwards_func(apps, schema_editor):
 def reverse_func(apps, schema_editor):
     LanguageBranches = apps.get_model("lexicon", "LanguageBranches")
     print('Discoloring LanguageBranches…')
-    for set in  setColors:
+    for set in setColors:
         try:
-            branch = LanguageBranches.objects.get(level1_branch_name=set['name'])
+            branch = LanguageBranches.objects.get(
+                level1_branch_name=set['name'])
             branch.hexColor = ''
             branch.save()
         except:
