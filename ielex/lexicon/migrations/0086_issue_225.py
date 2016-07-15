8a18b5bfbfcfff2e22fe3521fc7f65a4e1b59463
Jakob Runge <sicarius@g4t3.de>
2016-07-15 12:43:57 +0200

    Update code for #225

--- ielex/lexicon/migrations/0086_issue_225.py
+++ ielex/lexicon/migrations/0086_issue_225.py
@@ -10,41 +10,50 @@ def forwards_func(apps, schema_editor):
     '''
     Quickly written code for #225.
     '''
+    MeaningList = models.MeaningList
+    ml = MeaningList.objects.get(name=MeaningList.DEFAULT)
     Lexeme = models.Lexeme
+    lexemes = Lexeme.objects.filter(
+        meaning__in=ml.meanings.values_list('id', flat=True)).all()
     print("\n1: + lex. excl - Not swh")
-    for l in Lexeme.objects.filter(not_swadesh_term=False).all():
-        if l.is_excluded_lexeme:
-            print(l.id)
+    for l in lexemes:
+        if not l.not_swadesh_term:
+            if l.is_excluded_lexeme:
+                print(l.id)
     print("2: + lex .loan - Not swh")
-    for l in Lexeme.objects.filter(not_swadesh_term=False).all():
-        if l.is_loan_lexeme:
-            print(l.id)
+    for l in lexemes:
+        if not l.not_swadesh_term:
+            if l.is_loan_lexeme:
+                print(l.id)
     print("3: + lex. loan")
-    for l in Lexeme.objects.all():
+    for l in lexemes:
         if l.is_loan_lexeme:
             print(l.id)
     print("4: 2 >= cognate sets - Not swh")
-    for l in Lexeme.objects.filter(not_swadesh_term=False).all():
-        if l.cognatejudgement_set.count() >= 2:
-            print(l.id)
-    print("5: + cog. excl.")
-    for l in Lexeme.objects.all():
-        if l.is_excluded_cognate:
-            print(l.id)
-    print("6: + cog. loan")
-    for l in Lexeme.objects.all():
-        if l.is_loan_cognate:
-            print(l.id)
+    for l in lexemes:
+        if not l.not_swadesh_term:
+            if l.cognatejudgement_set.count() >= 2:
+                print(l.id)
+    print("5: + cog. excl. - Not swh")
+    for l in lexemes:
+        if not l.not_swadesh_term:
+            if l.is_excluded_cognate:
+                print(l.id)
+    print("6: + cog. loan - Not swh")
+    for l in lexemes:
+        if not l.not_swadesh_term:
+            if l.is_loan_cognate:
+                print(l.id)
     print(1/0)
 
 
 def reverse_func(apps, schema_editor):
-    print('Nothing to do for reverse_func of 0085_foo')
+    print('Nothing to do for reverse_func of #225')
 
 
 class Migration(migrations.Migration):
 
-    dependencies = [('lexicon', '0084_auto_20160713_1522')]
+    dependencies = [('lexicon', '0085_language_historical_init')]
 
     operations = [
         migrations.RunPython(forwards_func, reverse_func),
