d2f6fdb0dd226bcf4508594bc637f3ddf4517986
Jakob Runge <sicarius@g4t3.de>
2016-07-15 16:04:44 +0200

    Update code for #225

--- ielex/lexicon/migrations/0086_issue_225.py
+++ ielex/lexicon/migrations/0086_issue_225.py
@@ -10,11 +10,18 @@ def forwards_func(apps, schema_editor):
     '''
     Quickly written code for #225.
     '''
+    # Making sure we use `Jena200`
     MeaningList = models.MeaningList
     ml = MeaningList.objects.get(name=MeaningList.DEFAULT)
+    # Making sure we use `Current`
+    LanguageList = models.LanguageList
+    ll = LanguageList.objects.get(name=LanguageList.DEFAULT)
+    # Gathering lexemes to work with:
     Lexeme = models.Lexeme
     lexemes = Lexeme.objects.filter(
-        meaning__in=ml.meanings.values_list('id', flat=True)).all()
+        meaning__in=ml.meanings.values_list('id', flat=True),
+        language__in=ll.languages.values_list('id', flat=True)).all()
+    # Calculating:
     print("\n1: + lex. excl - Not swh")
     for l in lexemes:
         if not l.not_swadesh_term:
