7391385828d9ecc91c7b7aa009c2b335b8e52c7e
Jakob Runge <sicarius@g4t3.de>
2016-07-29 13:33:40 +0200

    Improve editCognateClass.js
    - Relates to #219, #51, #52
    - Adds means to add new cognate classes to a group of lexemes.
    - Adds saveguards against accidential addition of cognate classes or
      accidential grouping of lexemes that don't share cognate classes.
    - Only displays cognate classes that are shared across the selection.

--- static/js/editCognateClass.js
+++ static/js/editCognateClass.js
@@ -1,8 +1,8 @@
 (function(){
   "use strict";
-  return define(['jquery', 'lodash',
+  return define(['jquery', 'lodash', 'bootbox',
                  'js/gatherCheckboxValues'],
-                function($, _, gatherCheckboxValues){
+                function($, _, bootbox, gatherCheckboxValues){
     $('.editCognateClassButton').click(function(){
       //Gathering selected lexemes and their infos:
       var selected = gatherCheckboxValues('input.lexemeSelection');
@@ -12,44 +12,53 @@
       var lexemeIds = _.chain(selected).map(function(x){
         return x.lexemeId;
       }).uniq().value();
-      var cognateClassIds = _.chain(selected).map(function(x){
-        return x.cognateClassIds;
-      }).flattenDeep().uniq().value();
-      //Form to work with:
-      var form = $('#addToCognateClassForm');
-      //Cognate classes we know about:
-      var cognateClassMap = {}; // id -> {id, alias, root_form, root_language}
-      _.each(form.data('cognateclasses'), function(cc){
-        cognateClassMap[cc.id] = cc;
+      //Building a map from cognateClassIds to lexemeIds:
+      var cognateClassLexemeMap = {}; // cId -> {lexemeId -> true}
+      _.each(selected, function(x){
+        _.each(x.cognateClassIds, function(cId){
+          if(!(cId in cognateClassLexemeMap)){
+            cognateClassLexemeMap[cId] = {};
+          }
+          cognateClassLexemeMap[cId][x.lexemeId] = true;
+        });
       });
-      //Building the name for a cognate class:
-      var mkCognateClassName = function(cc, deli){
-        deli = deli || ' , ';
-        return _.filter([cc.alias,
-                         cc.root_form,
-                         cc.root_language], function(s){
-                           return !_.isEmpty(s);
-                         }).join(deli);
-      };
-      //Building options for a cognate class:
-      var mkOptions = function(cId){
-        return _.map(cognateClassMap, function(cc){
-          var selected = (cc.id === cId) ? ' selected="selected"' : '';
-          var name = mkCognateClassName(cc);
-          return '<option value="'+cc.id+'"'+selected+'>'+name+'</option>';
-        }).join('');
-      };
-      //Setting lexemeIds:
-      form.find('input[name="lexemeIds"]').val(lexemeIds.join(','));
-      //Providing editing for selected cognate classes:
-      _.each(cognateClassIds, function(cId){
-        //Current cognate class:
-        var current = cognateClassMap[cId];
-        //Composing cognate class name:
-        var name = mkCognateClassName(current, '<br>');
-        form.prepend(
-          '<div class="form-group cognateClassSelection" ' +
-               'data-for="'+current.id+'">' +
+      //Array of cognateClassIds with only cIds that belong to all lexemes:
+      var cognateClassIds = [];
+      _.each(cognateClassLexemeMap, function(lObj, cId){
+        if(_.keys(lObj).length === lexemeIds.length){
+          cognateClassIds.push(cId);
+        }
+      });
+      //Handling the modal:
+      var handleForm = function(){
+        //Form to work with:
+        var form = $('#addToCognateClassForm');
+        //Cognate classes we know about:
+        var cognateClassMap = {}; // id -> {id, alias, root_form, root_language}
+        _.each(form.data('cognateclasses'), function(cc){
+          cognateClassMap[cc.id] = cc;
+        });
+        //Building the name for a cognate class:
+        var mkCognateClassName = function(cc, deli){
+          deli = deli || ' , ';
+          return _.filter([cc.alias,
+                           cc.root_form,
+                           cc.root_language], function(s){
+                             return !_.isEmpty(s);
+                           }).join(deli);
+        };
+        //Building options for a cognate class:
+        var mkOptions = function(cId){
+          return _.map(cognateClassMap, function(cc){
+            var selected = (cc.id === cId) ? ' selected="selected"' : '';
+            var name = mkCognateClassName(cc);
+            return '<option value="'+cc.id+'"'+selected+'>'+name+'</option>';
+          }).join('');
+        };
+        //
+        var mkCognateClassSelection = function(cId, name){
+          return '<div class="form-group cognateClassSelection" ' +
+               'data-for="'+cId+'">' +
             '<label for="selectCognateClass"' +
                    'class="col-sm-3">' +
               name +
@@ -58,12 +67,49 @@
               '<select name="selectCognateClass">' +
                 '<option value="new">New</option>' +
                 '<option value="delete">Delete</option>' +
-                mkOptions(current.id) +
+                mkOptions(cId) +
               '</select>' +
             '</div>' +
-          '</div>'
-        );
-      });
+          '</div>';
+        };
+        //Setting lexemeIds:
+        form.find('input[name="lexemeIds"]').val(lexemeIds.join(','));
+        //Removing old selections:
+        form.find('.cognateClassSelection').remove();
+        //Providing editing for selected cognate classes:
+        _.each(cognateClassIds, function(cId){
+          //Current cognate class:
+          var current = cognateClassMap[cId];
+          //Composing cognate class name:
+          var name = mkCognateClassName(current, '<br>');
+          form.prepend(mkCognateClassSelection(current.id, name));
+        });
+        //Handling add button:
+        form.find('#addEntryCognateClassButton').click(function(){
+          var msg = 'Are you sure that you want to ' +
+                    'add an additional cognate class?';
+          bootbox.confirm(msg, function(result){
+            if(result === true){
+              form.prepend(mkCognateClassSelection('new', 'new'));
+            }
+          });
+        });
+        //Display modal:
+        $('#editCognateClassModal').modal('show');
+      };
+      //Checking if we've got a nice selection:
+      if(cognateClassIds.length === 0){
+        var msg = 'The lexemes in your selection share ' +
+                  'no common cognate class. ' +
+                  'Are you sure you want to continue?';
+        bootbox.confirm(msg, function(result){
+          if(result === true){
+            handleForm();
+          }
+        });
+      }else{
+        handleForm();
+      }
     });
   });
 })();
