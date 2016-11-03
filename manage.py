7c0f38d709d0b7e22cff0a115a8b99ee462cfcfd
Jakob Runge <sicarius@g4t3.de>
2016-11-03 16:47:18 +0100

    Refactor bibtex command
    - This commit refactors the changes to manage.py
      introduced by 68d66d1234227e590320fdac4b80214a9375b722.
      Adding custom commands to Django should be done
      via extending the BaseCommand class
      instead of adding to the manage.py code.
      This has the additional benefit that the command can be discovered
      and has an explanation what it's for.

--- manage.py
+++ manage.py
@@ -5,8 +5,4 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ielex.settings")
     from django.core.management import execute_from_command_line
-    if sys.argv[1] == 'bibtex':
-        from ielex.bibtex_import import *
-        bibtex_update(sys.argv[2])
-    else:
-        execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
