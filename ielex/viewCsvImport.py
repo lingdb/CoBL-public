7046e0fb201016924cca2a89ba2ac4ef155e296d
Jakob Runge <sicarius@g4t3.de>
2016-11-03 17:11:16 +0100

    Add decorators to ielex.viewCsvImport
    - The newly added decorators make sure, that:
      * A user importing .csv files must be a superuser.
    
      * Importing a .csv file will only work with a correct csrf token.
    
      * Exceptions thrown in the viewCsvImport function will be handled
        by the logExceptions decorator.
    
    - Relates to #315

--- ielex/viewCsvImport.py
+++ ielex/viewCsvImport.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import user_passes_test
 # import clldutils.dsv as dsv
 # from ielex.lexicon.models import Language, Lexeme, Meaning
 from ielex.shortcuts import render_template
+from ielex.utilities import logExceptions
 
 
+@user_passes_test(lambda u: u.is_superuser)
+@csrf_protect
+@logExceptions
 def viewCsvImport(request):
     return render_template(request, "admin/viewCsvImport.html", {})
