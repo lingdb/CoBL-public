8e64856f31a79df037423dbdd34114b44758c50a
Jakob Runge <sicarius@g4t3.de>
2016-11-04 12:27:17 +0100

    Enable file uploads in ielex.viewCsvImport
    - This commit is the first working version of file uploads in viewCsvImport.
      The next steps will now be to validate the input data
      and merge it with the database correctly.
    
    - Relates to #315

--- ielex/viewCsvImport.py
+++ ielex/viewCsvImport.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import user_passes_test
-# import clldutils.dsv as dsv
+
+import clldutils.dsv as dsv
+
 # from ielex.lexicon.models import Language, Lexeme, Meaning
 from ielex.shortcuts import render_template
 from ielex.utilities import logExceptions
@@ -11,4 +13,11 @@ from ielex.utilities import logExceptions
 @csrf_protect
 @logExceptions
 def viewCsvImport(request):
+
+    if request.method == 'POST' and 'CsvImportForm' in request.POST:
+        importMethod = request.POST['tableType']
+        file = request.FILES['csvFile']
+        csvDictReader = dsv.reader(file, dicts=True)
+        print('DEBUG', importMethod, list(csvDictReader))
+
     return render_template(request, "admin/viewCsvImport.html", {})
