lingdb/CoBL-public

commit 56cb50e0f54ef867c44ce68d23cf0825b18e3acf

Implement viewing / editing / removing / adding citations in `/cognateclasslist/` and `/meaning/`  (#166): - Citation columns for `CognateClassCitation` (`/cognateclasslist/` and `/meaning/`) and `LexemeCitation` (`/meaning/`). - Counter badges for the new columns. - Popup with inlines formset when clicked. - Dynamic formsets. - Autocomplete functionality for Source. - Universal handling to fit different Citation models (`CognateClassCitation` and `LexemeCitation`). - Inline formset functionality (add, edit, delete and save changes). - Counter badges dynamic `ajax` update.
	modified:   ielex/forms.py
	modified:   ielex/lexicon/models.py
	modified:   ielex/templates/snippets/citation_inline_form.html
	new file:   ielex/templates/snippets/sourcePopUpScript.html
	modified:   ielex/templates/view_cognateclass_editable.html
	modified:   ielex/templates/view_meaning.html
	modified:   ielex/views.py
	modified:   static/assets.json
	new file:   static/js/dynamicFormset.js
	modified:   static/js/main.js

ielex/lexicon/models.py
@@ -422,9 +422,9 @@ class Source(models.Model):
                            'number': 'issue', 'part': 'part',
                            'edition': 'edition', 'editor': 'editor'}
         rft_attrs_lst = []
-        rft_val_dict = {'book': 'book', 'article': 'journal',
-                        'expert': 'expert', 'online': 'online',
-                        'inbook': 'inbook', 'misc':'misc', '':''}
+        rft_val_dict = {'book': 'book', 'article': 'article',
+                        'expert': 'document', 'online': 'webpage',
+                        'inbook': 'bookitem', 'misc': 'document'}
         for key in bibtex_rft_dict:
             try:
                 typ = rft_val_dict[self.ENTRYTYPE]
@@ -1219,6 +1219,11 @@ class CognateClass(AbstractTimestamped):
 
     @property
     def lexemeCount(self):
+        return CognateClassCitation.objects.filter(cognate_class=self).\
+               count()
+
+    @property
+    def citationCount(self):
         return self.computeCounts()['lexemeCount']
 
     @property

ielex/templates/view_cognateclass_editable.html
@@ -158,7 +158,7 @@
                   </div>
                 </div>
               </th>
-              {# citation_title.html #}
+              {# citations_title.html #}
               <th style="width: 40px;">
                 <div class="vertical-text">
                   <div class="vertical-text__inner">
@@ -351,6 +351,8 @@
                   <span class="glyphicon glyphicon-sort"></span>
                 </div>
               </td>
+			  {# citations_filter.html #}
+			  <td></td>
               {# merge_filter.html #}
               <td>
                 <button type="button"
@@ -685,9 +687,10 @@
                     {{ cc.alias.data }}
                   </a>
                 </td>
+				{# citations_data.html #}
                 <td class="cogClassCit"
                     style="text-align: center; width: 40px;">
-                  <span class="badge openModal">
+				  <span class="badge openModal" model="CognateClass" id="{{ cc.idField.data }}">
 					{{ cc.object_data.citCount }}
 				  </span>
                 </td>
@@ -896,46 +899,6 @@
 </div>
 {% endblock %}
 {% block footerScripts%}
-<script>
-$(document).ready(function () {
-
-	/**** BUILD CITATIONS FORM *****/	
-	
-		function build_cit_form(data){
-		$('#viewCitContainer').append($(data));
-		$('#viewCit')[0].style.display = "block";
-		$('#citations tbody tr').formset();
-        //$('#citations tbody tr').formset({
-           // prefix: 'citations' //'{{ formset.prefix }}'
-        //});
-		return false
-	};
-	
-	/**** VIEW CITATIONS *****/	
-	
-	$(document.body).on('click', '.openModal', function() {
-		$(this).toggleClass('selected');
-		$('#viewCitContainer').empty();
-		var cogClassId = $(this).parent().siblings('.cogClassId').text().trim();
-		var postdata = {
-			'postType': 'viewCit',
-			'csrfmiddlewaretoken': $("{% csrf_token %}").attr('value'),
-			'cogClassId' : cogClassId
-		};
-		$.post("/cognateclasslist/", postdata, function (data) {
-			build_cit_form(data);
-		});
-		return false
-	});
-	
-	/**** CLOSE CITATIONS VIEW *****/	
-	
-	$(document.body).on('click', '.close', function() {
-		$('.openModal.selected').removeClass('selected');
-		$('#viewCit')[0].style.display = "none";
-		return false
-	});
-});
-</script>
+{% include "snippets/sourcePopUpScript.html" %}
 {% endblock %}
 {# vim: set ft=htmldjango nowrap shiftwidth=2 expandtab: #}

ielex/forms.py
@@ -1525,7 +1525,8 @@ class EditCognateClassCitationForm(forms.ModelForm):
         """
         exclude = self._get_validation_exclusions()
         # remove our previously excluded field from the list.
-        exclude.remove("cognate_class")
+        if 'cognate_class' in exclude:
+            exclude.remove("cognate_class")
         try:
             self.instance.validate_unique(exclude=exclude)
         except ValidationError as e:
@@ -1542,6 +1543,18 @@ class EditCognateClassCitationForm(forms.ModelForm):
 class EditCognateClassCitationInlineForm(EditCognateClassCitationForm):
     comment = forms.CharField(
         widget=forms.Textarea(attrs={'cols': 60, 'rows': 2}), required=False)
+
+
+class EditLexemeCitationInlineForm(forms.ModelForm):
+    comment = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': 60, 'rows': 2}), required=False)
+
+    class Meta:
+        model = LexemeCitation
+        fields = ["source", "pages", "comment"]
+        widgets = {
+            'source': autocomplete.ModelSelect2(url='source-autocomplete')
+        }
 
 
 class AddCitationForm(forms.Form):
@@ -1842,8 +1855,23 @@ LexemeFormSet = inlineformset_factory(
 
 CognateClassCitationFormSet = inlineformset_factory(
     CognateClass, CognateClassCitation,
-    form = EditCognateClassCitationInlineForm,
-    can_delete=False, fields=('source','pages','comment',), extra=0)
+    form=EditCognateClassCitationInlineForm,
+    can_delete=True, fields=('source', 'pages', 'comment',), extra=1)
+
+# IMPORTANT:
+# extra>=1 is required here by the js override that fixes
+# django-dynamic-formsets & django-autocomplete-light
+# incorrect behaviour, when used together.
+
+LexemeCitationFormSet = inlineformset_factory(
+    Lexeme, LexemeCitation,
+    form=EditLexemeCitationInlineForm,
+    can_delete=True, fields=('source', 'pages', 'comment',), extra=1)
+
+# IMPORTANT:
+# extra>=1 is required here by the js override that fixes
+# django-dynamic-formsets & django-autocomplete-light
+# incorrect behaviour, when used together.
 
 
 class AssignCognateClassesFromLexemeForm(WTForm):

ielex/templates/snippets/citation_inline_form.html
@@ -1,6 +1,6 @@
 <form id="citations" method="post" action="">
 {{ formset.non_form_errors.as_ul }}
-<table id="formset" class="form">
+<table id="formset" class="form" prefix="{{ formset.prefix }}">
 {% for form in formset.forms %}
   {% if forloop.first %}
   <thead><tr>
@@ -27,4 +27,23 @@
 {% endfor %}
 </table>
 {{ formset.management_form }}
+{% include "buttons/submit-cancel.html" %}
 </form>
+<div id="formTemplateContainer" style="display: none">
+<table>
+	<tr class="">
+	{% for field in formset.forms.0.visible_fields %}
+    <td>
+    {# Include the hidden fields in the form #}
+    {% if forloop.first %}
+      {% for hidden in form.hidden_fields %}
+      {{ hidden }}
+      {% endfor %}
+    {% endif %}
+      {{ field.errors.as_ul }}
+      {{ field }}
+    </td>
+  {% endfor %}
+	</tr>
+</table>
+</div>

ielex/templates/snippets/sourcePopUpScript.html
@@ -1,0 +1,82 @@
+<script>
+$(document).ready(function () {
+
+	/**** BUILD CITATIONS FORM *****/	
+	function build_cit_form(data){
+		
+		$('#viewCitContainer').append($(data));
+		formTemplate = $('#formTemplateContainer tr');
+		formTemplate.find('input:hidden[id $= "-DELETE"]').remove();
+		// Clear all cloned fields, except those the user wants to keep
+		formTemplate.find('input,select,textarea,label,div').each(function() {
+			var elem = $(this);
+			if (elem.is('input:checkbox') || elem.is('input:radio')) {
+				elem.attr('checked', false);
+			// consider forking django-dynamic-templates and adding this fix to clear autocomplete forms
+			} else if (elem.is('select[data-autocomplete-light-function="select2"]')) {
+				elem.parent().html('<select data-autocomplete-light-function="select2" data-autocomplete-light-url="/source-autocomplete/" id="id_citations-0-source" name="citations-0-source"/>');
+			} else {
+				elem.val('');
+			}
+		});
+        $('#citations tbody tr').formset({
+           prefix: 'citations',
+		   formTemplate: formTemplate,
+        });
+		$('#viewCitContainer form tr:last .delete-row').trigger("click");
+		$('#viewCit')[0].style.display = "block";
+		return false
+	};
+	
+	/**** VIEW CITATIONS *****/	
+	$(document.body).on('click', '.openModal', function() {
+		$(this).toggleClass('selected');
+		$('#viewCitContainer').empty();
+		var postdata = {
+			'postType': 'viewCit',
+			'csrfmiddlewaretoken': $("{% csrf_token %}").attr('value'),
+			'id' : $(this).attr('id'),
+			'model' : $(this).attr('model'),
+		};
+		$.post("/cognateclasslist/", postdata, function (data) {
+			build_cit_form(data);
+		});
+		return false
+	});
+	
+	/**** CLOSE CITATIONS VIEW *****/	
+	$(document.body).on('click', '.close', function() {
+		close_modal();
+		return false
+	});
+	
+	function close_modal(){
+		$('.openModal.selected').removeClass('selected');
+		$('#viewCit')[0].style.display = "none";
+	};
+
+	/**** SUBMIT FORM *****/
+	$(document).on("click", "#viewCitContainer :submit", function(e){ // catch the form's submit event
+		var postdata = {'postType': 'update',
+									'csrfmiddlewaretoken': $("{% csrf_token %}").attr('value'), 
+									'source_data': $('form#citations').serialize(),
+									'action': $(this).val(),
+									'id' : $('.openModal.selected').attr('id'),
+									'model' : $('.openModal.selected').attr('model'),
+									}
+		$.ajax({ // create an AJAX call...
+			data: postdata, // get the form data
+			type: $(this).parent().attr('method'), // GET or POST
+			url: $(this).parent().attr('action'), // the file to call
+			success: function(response) { // on success..
+				id = response['id']
+				model = response['model']
+				badgeUpdate = response['badgeUpdate']
+				$('.openModal[id= "'+ id +'"][model= "'+ model +'"]').text(badgeUpdate)
+			}
+		});
+		close_modal();
+		return false;
+	});
+});
+</script>

ielex/templates/view_meaning.html
@@ -7,7 +7,57 @@
 {% block title %}
   Mg: {{ meaning.gloss }}
 {% endblock %}
+{% block meta %}
+{{ block.super }}
+<style>
+/* The Modal (background) */
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
 
+/* Modal Content/Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    width: 1020px;
+}
+
+/* The Close Button */
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.openModal {
+	background-color: blue;
+	cursor: pointer;
+}
+
+.openModal.selected{
+	background-color: red;
+}
+</style>
+{% endblock %}
 {% block top %}
 {% endblock %}
 
@@ -68,7 +118,15 @@
             <th colspan="3"></th>
             {# transliteration_title.html #}
             <th>Translit.</th>
-            {# swadesh_title.html #}
+            {# citations_title.html #}
+            <th>
+              <div class="vertical-text">
+                <div class="vertical-text__inner">
+                  Lex. Citat
+                </div>
+              </div>
+            </th>
+			 {# swadesh_title.html #}
             <th>
               <div class="vertical-text">
                 <div class="vertical-text__inner">
@@ -111,6 +169,14 @@
             {% endif %}
             {# cognateclassids_title.html #}
             <th>Cog.<br>ID</th>
+            {# cognateclasscitations_title.html #}
+            <th>
+              <div class="vertical-text">
+                <div class="vertical-text__inner">
+                  Cog. Citat
+                </div>
+              </div>
+            </th>
             {# cognateclasses_title.html #}
             <th>
               Cog.<br>Alias
@@ -225,6 +291,9 @@
                 </div>
               {% endif %}
             </td>
+            {# citations_filter.html #}
+            <td>
+            </td>
             {# swadesh_filter.html #}
             <td>
               <button type="button"
@@ -267,6 +336,8 @@
                 <span class="glyphicon glyphicon-sort"></span>
               </div>
             </td>
+            {# cognateclasscitations_filter.html #}
+            <td></td>
             {# cognateclasses_filter.html #}
             <td>
               {% if user.is_authenticated %}
@@ -516,6 +587,12 @@
                   {{ lexeme.transliteration.data }}
                 {% endif %}
               </td>
+              {# citations_data.html #}
+              <td class="lexemeCit">
+					<span class="badge openModal" model="Lexeme" id="{{ lexeme.object_data.id }}">
+					{{ lexeme.object_data.LexCitCount }}
+					</span>
+			  </td>
               {# swadesh_data.html #}
               <td class="lexemeSwadeshTerm">
                 {% if user.is_authenticated %}
@@ -561,6 +638,14 @@
                      title="New tab to view details of this cognate set and all member lexemes.">
                     {{ c.id }}
                   </a>
+                {% endfor %}
+              </td>
+              {# cognateclasscitations_data.html #}
+              <td class="cognateClassCitation" style="text-align: center;">
+                {% for c in lexeme.object_data.allCognateClasses %}
+                  <span class="badge openModal" model="CognateClass" id="{{ c.id }}">
+				  {{ c.lexemeCount }}
+                  </span>
                 {% endfor %}
               </td>
               {# cognateclasses_data.html #}
@@ -714,5 +799,16 @@
       {% endif %}
     </div>
   </div>
+<!-- view Citations -->
+<div id="viewCit" class="modal">
+  <!-- Content -->
+  <div class="modal-content">
+    <span class="close">x</span>
+	<div id="viewCitContainer"></div>
+  </div>
+</div>
+{% endblock %}
+{% block footerScripts%}
+{% include "snippets/sourcePopUpScript.html" %}
 {% endblock %}
 {# vim: set ft=htmldjango nowrap shiftwidth=2 expandtab: #}

ielex/views.py
@@ -15,7 +15,8 @@ from django.core.urlresolvers import reverse
 from django.db import IntegrityError, transaction
 from django.db.models import Q
 from django.forms import ValidationError
-from django.http import HttpResponse, HttpResponseRedirect, Http404, QueryDict
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, \
+     Http404, QueryDict
 from django.shortcuts import redirect
 from django.template import RequestContext
 from django.template import Template
@@ -67,6 +68,7 @@ from ielex.forms import AddCitationForm, \
     CognateClassFormSet, \
     CognateClassCitationFormSet, \
     LexemeFormSet, \
+    LexemeCitationFormSet, \
     AssignCognateClassesFromLexemeForm
 from ielex.lexicon.models import Author, \
     Clade, \
@@ -1117,9 +1119,68 @@ def edit_meaning(request, meaning):
                             "form": form})
 
 
+@logExceptions
+def view_citations_inline_form(request, model, formset):
+    model_obj = model.objects.get(
+        id=request.POST.get("id"))
+    response = HttpResponse()
+    response.write(
+        render_to_string('snippets/citation_inline_form.html',
+                         context={'formset':
+                                  formset(
+                                      instance=model_obj,
+                                      prefix='citations')
+                                  },
+                         request=request)
+        )
+    return response
+
+
+@logExceptions
+def submit_citations_inline_form(request, model, formset):
+    rel_cit_field_dict = {'CognateClass': 'cognate_class','Lexeme': 'lexeme'}
+    modelcit = eval(request.POST.get("model")+'Citation')
+    rel_cit_field = rel_cit_field_dict[request.POST.get("model")]
+    model_obj = model.objects.get(
+        id=request.POST.get("id"))
+    formset = formset(
+        QueryDict(request.POST['source_data'].encode('ASCII')),
+        instance=model_obj,
+        prefix='citations'
+        )
+    if formset.is_valid():
+        formset.save()
+        filter_kwgrs = {rel_cit_field: model_obj.pk}
+        return JsonResponse({'id': request.POST.get("id"),
+                             'model': request.POST.get("model"),
+                             'badgeUpdate':
+                             modelcit.objects.filter(**filter_kwgrs).count()
+                             },
+                            )
+    else:
+        print(formset.errors())
+    return JsonResponse({})
+
+
+@login_required
+@logExceptions
+def citation_form_event(request):
+    if not request.POST.get("model"):
+        return None
+    model = eval(request.POST.get("model"))
+    formset = eval(request.POST.get("model")+'CitationFormSet')
+    if request.POST.get("postType") == 'viewCit':
+        return view_citations_inline_form(request, model, formset)
+    elif request.POST.get("action") == 'Submit':
+        return submit_citations_inline_form(request, model, formset)
+
+
 @csrf_protect
 @logExceptions
 def view_meaning(request, meaning, language_list, lexeme_id=None):
+    cit_form_response = citation_form_event(request)
+    if cit_form_response:
+        return cit_form_response
     setDefaultMeaning(request, meaning)
     if language_list is None:
         language_list = getDefaultLanguagelist(request)
@@ -1191,7 +1252,14 @@ def view_meaning(request, meaning, language_list, lexeme_id=None):
         "lexemecitation_set",
         "cognate_class",
         "language__languageclade_set",
-        "language__clades")
+        "language__clades").extra({'LexCitCount':
+                                   'SELECT COUNT(*) '
+                                   'FROM lexicon_lexemecitation '
+                                   'WHERE '
+                                   'lexicon_lexemecitation.'
+                                   'lexeme_id '
+                                   '= lexicon_lexeme.id',
+                                   })
     # Gather cognate classes and provide form:
     cognateClasses = CognateClass.objects.filter(lexeme__in=lexemes).distinct()
     cognate_form = ChooseCognateClassForm()
@@ -1230,27 +1298,12 @@ def view_meaning(request, meaning, language_list, lexeme_id=None):
          "clades": Clade.objects.all()})
 
 
-def view_cognateclasscitations(request):
-    cognateclass_obj = CognateClass.objects.get(
-        id=request.POST.get("cogClassId"))
-    response = HttpResponse()
-    response.write(
-        render_to_string('snippets/citation_inline_form.html',
-                         context={'formset':
-                                  CognateClassCitationFormSet(
-                                      instance=cognateclass_obj,
-                                      prefix='citations')
-                                  },
-                         request=request)
-        )
-    return response
-
-
 @csrf_protect
 @logExceptions
 def view_cognateclasses(request, meaning):
-    if request.POST.get("postType") == 'viewCit':
-        return view_cognateclasscitations(request)
+    cit_form_response = citation_form_event(request)
+    if cit_form_response:
+        return cit_form_response
     setDefaultMeaning(request, meaning)
     # Handle POST of AddCogClassTableForm:
     if request.method == 'POST':
@@ -2059,7 +2112,7 @@ def source_list(request):
 def get_sources_table(queryset=''):
     if queryset == '':
         queryset = Source.objects.all()
-    current_languagelist = get_canonical_language_list()
+    #current_languagelist = get_canonical_language_list()
     queryset = queryset.extra({'cognacy_count':
                                'SELECT COUNT(*) '
                                'FROM lexicon_cognatejudgementcitation '

static/js/dynamicFormset.js
@@ -1,0 +1,5 @@
+(function(){
+  "use strict";
+  return define(['django-dynamic-formset','jquery'], function(_, $){
+  });  
+})();

commit ddff7d21eca588b97cae60af134f118e206c875e

Fix citation counts for view_cognateclass_editable
- Relates to #384

- Part of this problem was introduced in 56cb50e0f.

ielex/lexicon/models.py
@@ -1197,6 +1197,8 @@ class CognateClass(AbstractTimestamped):
             if languageList is not None:
                 lSet = set(llo.language_id for llo in
                            languageList.languagelistorder_set.all())
+            else:
+                lSet = set()
             # Gather counts:
             lexemeCount = 0
             onlyNotSwh = True  # True iff all lexemes are not_swadesh_term.
@@ -1241,12 +1243,12 @@ class CognateClass(AbstractTimestamped):
 
     @property
     def lexemeCount(self):
-        return CognateClassCitation.objects.filter(cognate_class=self).\
-               count()
+        return self.computeCounts()['lexemeCount']
 
     @property
     def citationCount(self):
-        return self.computeCounts()['lexemeCount']
+        return CognateClassCitation.objects.filter(cognate_class=self).\
+               count()
 
     @property
     def hasOnlyNotSwadesh(self):

ielex/templates/view_cognateclass_editable.html
@@ -351,8 +351,8 @@
                   <span class="glyphicon glyphicon-sort"></span>
                 </div>
               </td>
-			  {# citations_filter.html #}
-			  <td></td>
+      			  {# citations_filter.html #}
+      			  <td></td>
               {# merge_filter.html #}
               <td>
                 <button type="button"
@@ -672,12 +672,12 @@
                     {{ cc.alias.data }}
                   </a>
                 </td>
-				{# citations_data.html #}
+        				{# citations_data.html #}
                 <td class="cogClassCit"
                     style="text-align: center; width: 40px;">
-				  <span class="badge openModal" model="CognateClass" id="{{ cc.idField.data }}">
-					{{ cc.object_data.citCount }}
-				  </span>
+        				  <span class="badge openModal" model="CognateClass" id="{{ cc.idField.data }}">
+        					{{ cc.object_data.citCount }}
+        				  </span>
                 </td>
                 {# merge_data.html #}
                 <td>