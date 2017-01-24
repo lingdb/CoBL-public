7384240da35d8caa65b0483b1e59e0a46be20203
Jakob Runge <sicarius@g4t3.de>
2017-01-24 14:22:28 +0100

    Bugfix in viewTableFilter/phoneticData.js
    - Relates to #160

--- static/js/viewTableFilter/phoneticData.js
+++ static/js/viewTableFilter/phoneticData.js
@@ -27,25 +27,27 @@
         stOther: 'ɓɗʄɠʛʘǀǃǂǁ'
       }
     };
-    module.replacements = {
-      // vowels
-      'V': '[' + module.vMust + '][' + module.vMay + ']*',
-      // consonants
-      'C': '[' + module.cMain + module.cOther + module.cAdditional + ']',
-      // nasal
-      'N': '[' + module.cNasal + module.vNasal + ']',
-      // All from lateral {friccative, approx.}
-      'FL': '[ɬɮʋɹɻjɰ]',
-      // selection by Paul
-      'R': '[ʋɹɻʀʁχrɾⱱ˞]',
-      // affricate
-      'A': '(tʃ|ts|tθ|ʈʂ|dʒ|dz|dð|ɖʐ|pf|bβ|kx|ɡɣ)',
-      // fricative, lateral friccative
-      'F': '[' + module.fMain + module.fOther + ']',
-      // others
-      'S': '[' + module.stMain + module.stOther + ']',
-      'T': '[' + module.stMain + module.stOther + ']'
-    };
+    module.replacements = (function(s){
+      return {
+        // vowels
+        'V': '[' + s.vMust + '][' + s.vMay + ']*',
+        // consonants
+        'C': '[' + s.cMain + s.cOther + s.cAdditional + ']',
+        // nasal
+        'N': '[' + s.cNasal + s.vNasal + ']',
+        // All from lateral {friccative, approx.}
+        'FL': '[ɬɮʋɹɻjɰ]',
+        // selection by Paul
+        'R': '[ʋɹɻʀʁχrɾⱱ˞]',
+        // affricate
+        'A': '(tʃ|ts|tθ|ʈʂ|dʒ|dz|dð|ɖʐ|pf|bβ|kx|ɡɣ)',
+        // fricative, lateral friccative
+        'F': '[' + s.fMain + s.fOther + ']',
+        // others
+        'S': '[' + s.stMain + s.stOther + ']',
+        'T': '[' + s.stMain + s.stOther + ']'
+      };
+    })(module.symbolGroups);
     var sanitizeInput = function(input){ // :: String -> String
       _.forEach(module.replacements, function(value, key){
         input = input.replace(new RegExp(key, 'g'), value);
@@ -55,8 +57,8 @@
     module.matches = function(input, text){
       // module.matches :: (input :: String, text :: String) -> Bool
       var match = text.replace(/[ˈˌ]/g, '')
-                      .match(new Regex(sanitizeInput(input), 'i'));
-      return match !== null;
+                      .match(new RegExp(sanitizeInput(input), 'i'));
+      return match === null;
     };
     return module;
   });
