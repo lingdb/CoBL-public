8cc9434148c2728c4f14afaf9d0dd92c53135dfb
Jakob Runge <sicarius@g4t3.de>
2017-01-30 14:27:40 +0100

    Update deployment/placeUnitFiles.sh

--- deployment/placeUnitFiles.sh
+++ deployment/placeUnitFiles.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
 cd /srv/cobl/deployment
 cp cobl.service cobl-{backup,nexus}.{service,timer} /etc/systemd/system/
 systemctl daemon-reload
