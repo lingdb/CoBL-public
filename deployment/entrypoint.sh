fc751de23f9fcebed76ee958a7e8f50b11be82a6
Jakob Runge <sicarius@g4t3.de>
2017-01-30 16:45:59 +0100

    Bugfix in deployment/entrypoint.sh

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -2,4 +2,4 @@
 cd /srv/cobl/
 pcount=$(grep -c "^processor" /proc/cpuinfo)
 wcount=$(./venv/bin/python -c "print($pcount * 2 + 1)")
-./venv/bin/gunicorn --workers $wcount --bind=$(hostname -i):5000 wsgi:application
+./venv/bin/gunicorn --workers $wcount --bind=localhost:5000 wsgi:application
