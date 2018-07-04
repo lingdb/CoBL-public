b8d1ea3e134977ed46adc23882077051c5c0408a
xrotwang <xrotwang@googlemail.com>
2018-07-04 09:47:43 +0200

    removed deployment directory, because the functionality moved to shh-dlce/appconfig

--- deployment/computeNexusExport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /srv/cobl/
-./venv/bin/python manage.py computeNexusExport
--- deployment/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /srv/cobl/
-pcount=$(grep -c "^processor" /proc/cpuinfo)
-wcount=$(./venv/bin/python -c "print($pcount * 2 + 1)")
-./venv/bin/gunicorn --workers $wcount --bind=localhost:5000 wsgi:application
